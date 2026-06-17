ff86bcbfe2622f3b83c2833d475cb5a44c43dcc9
mito710 <syuakai1121@gmail.com>
2026-06-17 13:16:06 +0900

    changed index

--- main.py
+++ main.py
@@ -15,14 +15,40 @@ async def root():
 @app.get("/index")
 def index():
     html_content = """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Look ma! HTML!</h1>
-        </body>
-    </html>
+    <!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<title>HOME</title>
+	<link rel="stylesheet" href="css/indexstyle.css">
+</head>
+
+<body>
+	<h1>~ 図書館を紹介 ~</h1>
+	<nav>
+		<ul>
+			<li><a href="index.html">HOME</a></li>
+			<li><a href="goodpoint.html">Good Point </a></li>
+			<li><a href="howto.html">How to</a></li>
+		</ul>
+	</nav>
+	<h4>24FI075のページへようこそ！</h4>
+	<div class="intro">
+		<h3>このサイトでは,<a class="intr" href="goodpoint.html">図書館のいいところ</a>や<a class="intr"
+				href="howto.html">おすすめな図書館の使い方</a>を紹介します
+			<br>
+			また、このサイトはスマホなどから見れないこともないです
+		</h3>
+	</div>
+	<div class="libe">
+		<img class="homeimg" src="images/lib1.jpg" alt="図書館内の画像">
+		<p class="imagetext">図書館のせかいへ</p>
+	</div>
+	<h6>画像：pngtree.com</h6>
+</body>
+
+</html>
     """
     return HTMLResponse(content=html_content, status_code=200)
 

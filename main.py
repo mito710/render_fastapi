e04bb2df601c506265a8728d6fcf45b7a8241db4
mito710 <syuakai1121@gmail.com>
2026-06-17 13:01:21 +0900

    add index

--- main.py
+++ main.py
@@ -11,6 +11,20 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
